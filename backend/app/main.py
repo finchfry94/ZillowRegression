c10203aa737027ec593545c2422a33e7670b1a14
finchfry94 <nicholas.finch05@gmail.com>
2025-03-29 08:58:24 -0400

    docs

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ if settings.SENTRY_DSN and settings.ENVIRONMENT != "local":
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
-    docs_url='/docs'
+    docs_url='/docs',
     generate_unique_id_function=custom_generate_unique_id,
 )
 
