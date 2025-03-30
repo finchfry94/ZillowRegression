f2ac286ed52df2e39a206d88d4e302397d3c1c6e
finchfry94 <nicholas.finch05@gmail.com>
2025-03-29 21:56:14 -0400

    taks are being run by celery! I had the wrong queue name

--- backend/app/worker.py
+++ backend/app/worker.py
@@ -7,8 +7,14 @@ celery_app = Celery(
     include=['app.tasks']
 )
 
-celery_app.conf.task_routes = {"app.tasks.*": {"queue": "default"}}
+celery_app.conf.task_routes = {"app.tasks.*": {"queue": "celery"}}
 
 # Enable Celery events
 celery_app.conf.worker_send_task_events = True
 celery_app.conf.task_send_sent_event = True
+
+# Add the new setting for broker connection retries on startup
+celery_app.conf.broker_connection_retry_on_startup = True
+celery_app.conf.broker_connection_max_retries = 100  # Retry up to 100 times
+celery_app.conf.broker_connection_retry_backoff = 2  # Wait 2 seconds between retries
+celery_app.conf.broker_connection_retry_backoff_max = 30 # wait a maximum of 30 seconds between retries
